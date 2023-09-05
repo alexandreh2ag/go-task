094cd3de6b4ce01266936a8fda8e6954123ffc96
jpabegg <abegg.jp@hotmail.fr>
2023-09-05 10:42:17 +0200

    add test for package worker
    Signed-off-by: jpabegg <abegg.jp@hotmail.fr>

--- cli/worker/generate_test.go
+++ cli/worker/generate_test.go
@@ -1,1 +1,47 @@
 package worker
+
+import (
+	"alexandreh2ag/go-task/cli/flags"
+	"alexandreh2ag/go-task/context"
+	mockOs "alexandreh2ag/go-task/mocks/os"
+	mockAfero "alexandreh2ag/go-task/mocks/spf13"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestGetWorkerGenerateCmd_Success(t *testing.T) {
+	ctx := context.TestContext(io.Discard)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	fsMock := mockAfero.NewMockFs(ctrl)
+	fileMock := mockAfero.NewMockFile(ctrl)
+	ctx.Fs = fsMock
+
+	outputDir := "/tmp/subdir"
+	outputPath := outputDir + "/output.txt"
+
+	dirLogMock := mockOs.NewMockFileInfo(ctrl)
+	fsMock.EXPECT().Stat(gomock.Eq(outputDir)).Times(1).Return(dirLogMock, nil)
+	fsMock.EXPECT().Create(gomock.Eq(outputPath)).Times(1).Return(fileMock, nil)
+	fileMock.EXPECT().Write(gomock.Any()).Times(5).Return(1, nil)
+
+	cmd := GetWorkerGenerateCmd(ctx)
+	cmd.SetArgs([]string{"--" + flags.GroupName, "test", "--" + OutputPath, outputPath})
+	err := cmd.Execute()
+	assert.NoError(t, err)
+}
+
+func TestGetWorkerGenerateCmd_MissingArgs(t *testing.T) {
+	ctx := context.TestContext(io.Discard)
+
+	cmd := GetWorkerGenerateCmd(ctx)
+	cmd.SetOut(io.Discard)
+	cmd.SetErr(io.Discard)
+	err := cmd.Execute()
+	assert.NotEqual(t, err, nil)
+	assert.Equal(t, true, strings.Contains(err.Error(), "missing mandatory arguments"))
+}
